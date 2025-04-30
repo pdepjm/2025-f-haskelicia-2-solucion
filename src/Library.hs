9f48e7035518df3394f91d561b65f6a8b1ff123a
Elin Winter <elinlwinter1@gmail.com>
2025-04-30 11:13:06 -0300

    comentarios básicos, falta ponerlo lindo

--- src/Library.hs
+++ src/Library.hs
@@ -25,6 +25,19 @@ type Brujo = SoldadoBlanco
 type Capitan = SoldadoBlanco
 type FuerzasBlancas = (Capitan, Brujo)
 
+-- Otra opción, completamente válida, 
+-- para modelar a los soldados blancos, es usar DATA.
+{-
+
+data SoldadoBlanco = UnSoldadoBlanco {
+  nombre :: Nombre,
+  habilidades :: [Habilidad]
+}
+
+-}
+-- Aquí lo modelamos como tupla, así podína tener un ejemplo
+-- de como lo usamos apropiadamente, de forma oficial. 
+
 -- --------------- Definicion de Tipos
 
 type Nivel = Number
@@ -39,6 +52,18 @@ data Palo = Corazon | Pica | Diamante | Trebol
 
 data RolCombate = Curador | Guerrero | Explosivo | Espia
   deriving (Eq, Show)
+
+-- Otra opción para modelar palo y rol de combate es usar String,
+-- para nosotros la utilización de Múltiples Constructores es mejor. 
+-- Permite evitar errores humanos de escritura. Al usar Strings, podríamos
+-- tener en el sistema "espia" y espía", o "Diamante" y "diamante".
+-- Al no tener un consenso de como se escribe, es más fácil confundirse en la escritura. 
+-- No solo eso, sino que al crear un Múltiple Constructor evitamos que
+-- en la utilización del sistema, alguien diga que un palo es "basto"
+-- podría ser un error común, y los strings lo permitirían, los Múltiples
+-- Constructores no. 
+-- No es un error usar Strings, pero por sus posibels errores y fallas,
+-- recomendmos usar Múltiples Constructores. 
 
 -- --------------- Ejemplos
 
@@ -58,13 +83,13 @@ carta4 = UnaCartaMagica 7 Trebol Inactiva
 ejercitoReinaRoja :: FuerzasRojas
 ejercitoReinaRoja = [carta1, carta2, carta3, carta4]
 
-daliaCentenaria :: Capitan
+daliaCentenaria :: SoldadoBlanco
 daliaCentenaria = ("daliaCentenaria", ["espada de petalo", "daga espinosa", "lanzador de abono"])
 
-rosaEnvidiosa :: Capitan
+rosaEnvidiosa :: SoldadoBlanco
 rosaEnvidiosa = ("rosaEnvidiosa", ["daga espinosa", "escudo rojo pasión", "rifle ak16"])
 
-gatoDeCheshire :: Brujo
+gatoDeCheshire :: SoldadoBlanco
 gatoDeCheshire = ("gatoChesire", ["desaparecetus", "abrete sesamo", "abracadabra"])
 
 sombrerero :: SoldadoBlanco
@@ -72,6 +97,29 @@ sombrerero = ("sombrerero", [])
 
 ejercitoReinaBlanca :: FuerzasBlancas
 ejercitoReinaBlanca = (daliaCentenaria, gatoDeCheshire)
+
+-- Noten como dalia centenariaa y Gato de Chesire estan modelados como ejemplos,
+-- como instancias de soldado blanco, de la misma forma qu rosa envidiosa
+-- o sombrerero. NO LOS MODELO como las únicas opciones del ejerciot de la reina blanca.
+-- Son ambos soldados blancos, y aunque ahora formna parte de las fuerzas blancas, 
+-- en el futuro tal vez no, y debo saber modelarlo correctamente. 
+-- Sería un error hacer esto: 
+
+{-
+
+data DaliaCentenaria = UnSoldadoBlanco {
+  nombre :: Nombre,
+  habilidades :: [armas]
+}
+
+-}
+
+-- EN vez del data o las tuplas de Soldado Blanco.
+-- Lo mismo si hago esto:
+-- type EjercitoBlanco = (daliaCentenaria, gatoDeCheshire)
+-- En vez de lo que aparece en esta solucion liberada. 
+-- El enunciado dice especificamnete que en el futuro podría cambiar
+-- por lo que hay que tenerlo en cuenta al modelar. 
 
 -- -----------------------------------------------------------------------------
 -- --------------- PARTE II: PREPARANDO CAMPO DE BATALLA ---------------------------------
@@ -133,6 +181,16 @@ cantMulta = 3
 bonusCantHabilidades :: [Habilidad] -> Number
 bonusCantHabilidades habilidades = bonusCantBase habilidades - multaSobrepeso habilidades
 
+{-
+energiaPorCantArmas :: [String] -> Number
+energiaPorCantArmas armas
+    | length armas <= 3 = length armas * 5
+    | otherwise = 3 * 5 - (length armas - 3) * 3
+
+Recordar la forma en la que resolvimos los pinos en el TP anterior! No está mal pero en este caso podría ser más expresiva y declarativa
+
+-}
+
 bonusCantBase :: [Habilidad] -> Number
 bonusCantBase habilidades = min (length habilidades) cantBaseHabilidades * puntosBase
 
@@ -143,6 +201,26 @@ bonusElemento :: String -> [Habilidad] -> Number
 bonusElemento eltoClave habilidades
   | elem eltoClave habilidades = 10
   | otherwise = 0
+
+-- Noten como teniamos la posibilidad de hacer algo así:
+{-
+tieneEspadaDePetalo :: DaliaCentenaria -> Bool
+tieneEspadaDePetalo dalia = "Espada de petalo" `elem` armas dalia
+
+tieneDesaparecetus :: GatoDeCheshire -> Bool
+tieneDesaparecetus gato = "Desaparecetus" `elem` hechizos gato
+-}
+
+-- Esa solución, aunque es mas declarativa y expresiva, REPITE MUCHO CODIGO. 
+-- Recuerden la clase, cada vez que no se reutiliza codigo, muere un gatito. 
+-- Se evalua en los examanes por su reutilizacion de codigo, demuestra que 
+-- conocen su codigo completo y que pueden pensar criticamnte sobre como pogramar. 
+-- Deben estar entrenados para spotear stas instancias donde el enunciado
+-- implicitamente, pide exactamente lo mismo en dos lados **muchas veces
+-- es trampita para poder evaluar la reutilizacion de codigo**. 
+-- Tienen que darse cuenta que tiene espada de petalo y tiene desaparacetus
+-- ambos evaluan si se encuentra un string en una lista de strings, mas alla
+-- si unas son armas y la otra hechizos. 
 
 -- --------------- Poder Mágico
 
@@ -158,6 +236,30 @@ rolParaDiamante :: CartaMagica -> RolCombate
 rolParaDiamante carta
   | nivel carta > 5 = Explosivo
   | otherwise = Guerrero
+
+{-
+if nivel > 5 then Explosivo else Guerrero
+
+Aunque no está mal usar if then else, y en otras cursadas encima se lo da en clase, aquí NO lo daremos como tema y NO esperamos que lo usen. Si requieren condicionales, recomendamos usar GUARDAS. 
+
+Lo mismo con otras posibilidades del lenguaje como WHERE, LET, CASE, IN, MAYBE, en esta cursada NO lo vemos y NO lo usamos, NO usarlos en su solución. 
+
+-}
+
+{-
+determinarRol :: Palo -> Number -> Estado -> String
+determinarRol Corazon _ _ = "Curador"
+determinarRol Pica _ _ = "Guerrero"
+determinarRol Diamante nivel _
+    | nivel > 5 = "Explosivo"
+    | otherwise = "Guerrero"
+determinarRol Trebol _ estado
+    | estado == Activo = "Espía"
+    | otherwise = "Guerrero"
+
+otra opción para rol combate.
+
+-}
 
 rolParaTrebol :: CartaMagica -> RolCombate
 rolParaTrebol carta
@@ -192,6 +294,38 @@ poderMagicoReinaRoja [] = 0
 poderMagicoReinaRoja [carta] = 3 * poderMagicoCarta carta
 poderMagicoReinaRoja cartas = poderMagicoCarta (head cartas) + poderMagicoCarta (cartas !! 1) + poderMagicoCarta (last cartas)
 
+{-
+poderMagicoReinaRoja :: [Carta] -> Number
+poderMagicoReinaRoja cartas 
+    | length cartas == 1 = poderMagicoCarta (head cartas) * 3
+    | otherwise = suma de poderes de cartas. 
+
+Esto como vemos en la solución puede resolverse de forma mas clara con PATTERN MATCHING
+-}
+
+{-
+poderPorEstadoInactivo :: Number
+poderPorEstadoInactivo = 0
+
+poderBasePorRolExplosivo :: Number
+poderBasePorRolExplosivo = 12
+
+poderBasePorRolGuerrero :: Number
+poderBasePorRolGuerrero = 8
+
+poderBasePorRolEspia :: Number
+poderBasePorRolEspia = 7
+nroARestarleElNivelDeEspia :: Number
+nroARestarleElNivelDeEspia = 13
+menosPoderPorSerEspiaMuyBueno :: Number
+menosPoderPorSerEspiaMuyBueno = 30
+
+poderBasePorRolCurador :: Number
+poderBasePorRolCurador = 0.5
+
+
+no siempre es correcto colocar MUCHAS constantes para hacer mas expresivo el codigo, hay que hacer un balance
+-}
 
 -- --------------- Infiltracion Encubierta
 
@@ -216,6 +350,10 @@ cambiarPalo carta nuevoPalo = carta{palo = nuevoPalo}
 
 cambiarNivel :: CartaMagica -> Number -> CartaMagica
 cambiarNivel carta nuevoNivel = carta{nivel = nuevoNivel}
+
+-- Como se va a cambiar el palo y el nivel en VARIAS partes del programa
+-- entonces, abstraigo su logica en una funcion auxiliar. 
+-- Permite ser mas declarativos, expresivos y reutilizr codigo. 
 
 -- Aunque uno podría estar tentado a hacer una función 
 -- cambiaProp :: CartaMagica -> a -> b -> CartaMagica
@@ -287,6 +425,26 @@ formacionBalanceada ejercito = esPar ejercito && rolesDif ejercito
 esPar :: [CartaMagica] -> Bool
 esPar ejercito = even (length ejercito)
 
+-- Recordamos que esto es un incorrecto uso de guardas:
+{-
+esCantidadDeCartasPar :: [Carta] -> Bool
+esCantidadDeCartasPar cartas
+    | even (length cartas) = True
+    | otherwise= False
+-}
+
+
 rolesDif :: [CartaMagica] -> Bool
 rolesDif ejercito = 
   rolEnCombate (head ejercito) /= rolEnCombate (last ejercito)
+
+{-
+verificarNuevoPoderMagicoYAsignarPalo :: Number -> CartaMagica -> CartaMagica
+
+intercambiarRolesDeEjercitoReinaBlanca :: EjercitoReinaBlanca -> EjercitoReinaBlanca
+
+nroPrimerasLetrasDeNombreCoincidentes :: Number
+
+Generar código expresivo no siempr significa agregar mas palabras, sino que a una mirada rapida o semi rapida, l gente entienda el codigo. En estos casos, la cantidad de palabras es contraproducente, lo mismo con constantes de mas
+-}
+  
